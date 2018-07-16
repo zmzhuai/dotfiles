cc807864c7a35fc4f3ca6a307fe06dc431c494ec
Min <>
2018-07-16 13:11:38 +0800

    修改插件管理器更新脚本bug

--- .vim/update.sh
+++ .vim/update.sh
@@ -1,4 +1,6 @@
 if [ -e "$HOME/.vim/autoload/plug.vim" ]; then
+
+else
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
