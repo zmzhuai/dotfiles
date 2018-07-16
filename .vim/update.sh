afa63fadca66cc49d07455103872897b23920d0d
Min <>
2018-07-16 13:17:45 +0800

    修复vim插件管理器更新脚本错误

--- .vim/update.sh
+++ .vim/update.sh
@@ -1,6 +1,4 @@
-if [ -e "$HOME/.vim/autoload/plug.vim" ]; then
-
-else
+#if [ -e "$HOME/.vim/autoload/plug.vim" ]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
+#fi
